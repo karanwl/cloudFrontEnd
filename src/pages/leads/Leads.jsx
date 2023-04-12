ac5d2c7df78b1064d6ba5e1d3f620eba4f224738
Ronald Saenz <rjsaenz@espol.edu.ec>
2023-04-12 01:33:22 -0400

    Login and List Leads

--- src/pages/leads/Leads.jsx
+++ src/pages/leads/Leads.jsx
@@ -1,6 +1,17 @@
+import React,{useState,useEffect} from 'react';
 import "./Leads.css";
 
 export default function Leads() {
+  
+  const [leads, setLeads] = useState([]);
+
+  useEffect(() => {
+    fetch("https://raw.githubusercontent.com/karanwl/cloudFrontEnd/main/datajson/leads.json")
+      .then((response) => response.json())
+      .then((data) => setLeads(data.leads_obj));
+  }, []);
+
+
   return (
       <div class="leads-content">
         <h1>Leads List</h1>
@@ -9,41 +20,36 @@ export default function Leads() {
         </a>
         <div className="leads-table">
           <table>
-            <tr>
-              <th>Name</th>
-              <th>Primary Phone Number</th>
-              <th>Secondary Phone Number</th>
-              <th>Email Address</th>
-              <th>Company Website</th>
-              <th>Company Address</th>
-              <th>Actions</th>
-            </tr>
-            <tr>
-              <td>lorem ipsum</td>
-              <td>lorem ipsum</td>
-              <td>lorem ipsum</td>
-              <td>lorem ipsum</td>
-              <td>lorem ipsum</td>
-              <td>lorem ipsum</td>
-              <td>
-                <a href="/viewlead">View</a>
-                <a href="/editlead">Edit</a>
-                <a href="/deletelead">Delete</a>
-              </td>
-            </tr>
-            <tr>
-              <td>lorem ipsum</td>
-              <td>lorem ipsum</td>
-              <td>lorem ipsum</td>
-              <td>lorem ipsum</td>
-              <td>lorem ipsum</td>
-              <td>lorem ipsum</td>
-              <td>
-                <a href="/viewlead">View</a>
-                <a href="/editlead">Edit</a>
-                <a href="/deletelead">Delete</a>
-              </td>
-            </tr>
+            <thead>
+              <tr>
+                <th>Company Name</th>
+                <th>Person Name</th>
+                <th>Company Website</th>
+                <th>Company Address</th>
+                <th>Email Address</th>
+                <th>Phone Number</th>
+                <th>Username</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {leads.map((lead) => (
+                <tr key={lead.id}>
+                  <td>{lead.company_name}</td>
+                  <td>{lead.person_name}</td>
+                  <td>{lead.company_website}</td>
+                  <td>{lead.company_address}</td>
+                  <td>{lead.email_address}</td>
+                  <td>{lead.telephone_numbers}</td>
+                  <td>{lead.username}</td>
+                  <td>
+                    <a href={`/viewlead/${lead.id}`}>View</a>
+                    <a href={`/editlead/${lead.id}`}>Edit</a>
+                    <a href={`/deletelead/${lead.id}`}>Delete</a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </div>
       </div>
